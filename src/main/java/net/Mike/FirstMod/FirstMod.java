5c266d2b7f5a4b5b4e07141f3bd2c03918b61109
mikeyboy52 <mikevanlommel2007@gmai.com>
2024-12-10 18:47:34 +0100

    added custom Creative Mode Tabs

--- src/main/java/net/Mike/FirstMod/FirstMod.java
+++ src/main/java/net/Mike/FirstMod/FirstMod.java
@@ -1,6 +1,7 @@
 package net.Mike.FirstMod;
 
 import net.Mike.FirstMod.block.ModBlocks;
+import net.Mike.FirstMod.item.ModCreativeModeTabs;
 import net.Mike.FirstMod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -36,6 +37,8 @@ public class FirstMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
---
+++ src/main/java/net/Mike/FirstMod/item/ModCreativeModeTabs.java
@@ -0,0 +1,40 @@
+package net.Mike.FirstMod.item;
+
+import net.Mike.FirstMod.FirstMod;
+import net.Mike.FirstMod.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, FirstMod.MOD_ID);
+
+    public static final Supplier<CreativeModeTab> FIRST_MOD_ITEMS_TAB = CREATIVE_MODE_TAB.register("first_mod_items_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.BISMUTH.get()))
+                    .title(Component.translatable("creativetab.firstmod.firstmod_items"))
+                    .displayItems((ItemDisplayParameters, output) -> {
+                        output.accept(ModItems.BISMUTH);
+                        output.accept(ModItems.BORION);
+                    }).build());
+
+    public static final Supplier<CreativeModeTab> FIRST_MOD_BLOCK_TAB = CREATIVE_MODE_TAB.register("first_mod_blocks_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.BISMUTH_BLOCK))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(FirstMod.MOD_ID, "first_mod_items_tab"))
+                    .title(Component.translatable("creativetab.firstmod.firstmod_blocks"))
+                    .displayItems((ItemDisplayParameters, output) -> {
+                      output.accept(ModBlocks.BISMUTH_BLOCK);
+                      output.accept(ModBlocks.BISMUTH_ORE);
+                    }).build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TAB.register(eventBus);
+    }
+
+}
